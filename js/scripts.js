4297668e44b4f9e58c59b935f46c3fe0e310a12b
gg-mg <ggichuki2017@gmail.com>
2019-12-04 13:14:04 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+    $("div#click-one").click(function(event) {
+      whatToSay = "Hello!";
+      alert(whatToSay);
+    });
+
+    $("div#click-two").click(function(event) {
+      alert(whatToSay);
+    });
+  });
+
+//   $(document).ready(function() {
+
+    // $("div#click-one").click(function(event) {
+    //   var whatToSay = "Hello!";
+    //   alert(whatToSay);
+    // });
+
+    // $("div#click-two").click(function(event) {
+    //   alert(whatToSay);
+    // });
+//   });
+// THE CODE BELOW DOES NOT WORK
